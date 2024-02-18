ae9e04a88642712a501135b84463038a1d34ca08
shenmadu <tashen.madu@gmail.com>
2024-02-18 10:27:46 +0530

    set color of buttons and text

--- facebook-clone/src/App.jsx
+++ facebook-clone/src/App.jsx
@@ -5,17 +5,17 @@ import './App.css'
 
 
 function App() {
-  return (
-    <>
-      <div className="container col-xl-10 col-xxl-8 px-4">
+  return (    
+    <div className="container-flex bg-body-tertiary">
+      <div className="container col-xl-10 col-xxl-8 px-4 ">
         <div className="row align-items-center g-lg-5 py-5">
           <div className="col-lg-7 text-center text-lg-start">
-            <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">faceBook</h1>
-            <p className="col-lg-10 fs-4">Facebook helps you connect and share with the people in your life.</p>
+            <h1 className="display-4 fw-bold lh-1  mb-3" style={{ color: '#316FF6' }} >facebook</h1>
+            <p className="col-lg-10 fs-3">Facebook helps you connect and share with the people in your life.</p>
           </div>
-          <div className="col-md-10 mx-auto col-lg-5">
-            <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-              <div className="form-floating mb-3">
+          <div className="col-md-10  col-lg-5">
+            <form className="p-4 p-md-3 border rounded-3 bg-white mx-auto">
+              <div className="form-floating mb-2">
                 <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
                 <label for="floatingInput">Email address or phone number </label>
               </div>
@@ -23,19 +23,19 @@ function App() {
                 <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
                 <label for="floatingPassword">Password</label>
               </div>
-              <div className="checkbox mb-3">
-                <label>
-                
-                </label>
-              </div>
-              <button className="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
+              <div className="mx-auto text-center">
+              
+              <button className="w-100 btn btn-lg btn-primary mb-2" type="submit">Log in</button>
+              <a href="">Fogotten password?</a>
               <hr className="my-4" />
-              <small className="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+              <button className="w-60 btn btn-lg btn-success account-btn" type="submit">Create new account</button>
+              </div>
             </form>
           </div>
         </div>
       </div>
-    </>
+      </div>
+   
   )
 }
 
