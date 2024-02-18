4c56bbb9f52fccbb75f8c597e9ccb43c7de60826
shenmadu <tashen.madu@gmail.com>
2024-02-18 09:42:24 +0530

    create page using bootstrap

--- facebook-clone/src/App.jsx
+++ facebook-clone/src/App.jsx
@@ -3,31 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container col-xl-10 col-xxl-8 px-4">
+        <div className="row align-items-center g-lg-5 py-5">
+          <div className="col-lg-7 text-center text-lg-start">
+            <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">faceBook</h1>
+            <p className="col-lg-10 fs-4">Facebook helps you connect and share with the people in your life.</p>
+          </div>
+          <div className="col-md-10 mx-auto col-lg-5">
+            <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+              <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Email address or phone number </label>
+              </div>
+              <div className="form-floating mb-3">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Password</label>
+              </div>
+              <div className="checkbox mb-3">
+                <label>
+                
+                </label>
+              </div>
+              <button className="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
+              <hr className="my-4" />
+              <small className="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
